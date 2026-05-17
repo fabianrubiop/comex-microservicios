246458505871f46418fd47a1cca9522d0b774015
fabianrubio <fa.rubiop@duocuc.cl>
2026-05-17 10:19:47 -0400

    se agrego clasificacion entity, dtos, rsc, enums, application.properties, archivo sql para crear tabla clasificaciones en db.migration

---
+++ clasificacion-ms/clasificacion-ms/src/main/java/com/aduanas/comex/clasificacionms/repository/ClasificacionRepository.java
@@ -0,0 +1,8 @@
+package com.aduanas.comex.clasificacionms.repository;
+
+
+import com.aduanas.comex.clasificacionms.entity.Clasificacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClasificacionRepository extends JpaRepository<Clasificacion, Long> {
+}
