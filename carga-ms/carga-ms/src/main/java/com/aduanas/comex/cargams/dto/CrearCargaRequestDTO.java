b1a7d514684234ebd115594c5862c12d21d19ae0
fabianrubio <fa.rubiop@duocuc.cl>
2026-05-17 00:06:59 -0400

    agregar CargaService, configuracion para levantar base de datos en application.properties

--- carga-ms/carga-ms/src/main/java/com/aduanas/comex/cargams/dto/CrearCargaRequestDTO.java
+++ carga-ms/carga-ms/src/main/java/com/aduanas/comex/cargams/dto/CrearCargaRequestDTO.java
@@ -1,6 +1,7 @@
 package com.aduanas.comex.cargams.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -8,29 +9,31 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class CrearCargaRequestDTO {
 
-    @NotBlank
+    @NotBlank(message = "EL NUMERO DE DECLARACION NO PUEDE ESTAR VACIO")
     @Size(max = 50)
     private String nroDeclaracion;
 
-    @NotBlank
+    @NotBlank(message = "DEBE INCLUIR UNA DESCRIPCION")
     @Size(max = 255)
     private String descripcion;
 
-    @NotBlank
+    @NotNull(message = "EL PAIS DE ORIGEN ES OBLIGATORIO")
     @Size(max = 100)
     private String paisOrigen;
 
-    @NotBlank
+    @NotNull(message = "DEBE INGRESAR VALOR DECLARADO DE CARGA")
     @Positive
-    private String valorDeclarado;
+    private BigDecimal valorDeclarado;
 
-    @NotBlank
+    @NotNull(message = "RUT DE IMPORTADOR ES OBLIGATORIO")
     @Size(max = 12)
     private String importadorRut;
 
--- carga-ms/carga-ms/src/main/java/com/aduanas/comex/cargams/service/CargaService.java
+++ carga-ms/carga-ms/src/main/java/com/aduanas/comex/cargams/service/CargaService.java
@@ -1,4 +1,32 @@
 package com.aduanas.comex.cargams.service;
 
+import com.aduanas.comex.cargams.client.ClasificacionClient;
+import com.aduanas.comex.cargams.dto.CrearCargaRequestDTO;
+import com.aduanas.comex.cargams.dto.CrearCargaResponseDTO;
+import com.aduanas.comex.cargams.entity.Carga;
+import com.aduanas.comex.cargams.enums.EstadoCarga;
+import com.aduanas.comex.cargams.repository.CargaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class CargaService {
+
+    private final CargaRepository cargaRepository;
+    private final ClasificacionClient clasificacionClient;
+
+    public CrearCargaResponseDTO crear(CrearCargaRequestDTO dto){
+
+        Carga carga = Carga.builder().nroDeclaracion(dto.getNroDeclaracion()).descripcion(dto.getDescripcion()).paisOrigen(dto.getPaisOrigen()).valorDeclarado(dto.getValorDeclarado()).importadorRut(dto.getImportadorRut()).estado(EstadoCarga.REGISTRADA).fechaCreacion(LocalDateTime.now()).build();
+
+        carga = cargaRepository.save(carga);
+
+
+
+    }
+
+
 }
