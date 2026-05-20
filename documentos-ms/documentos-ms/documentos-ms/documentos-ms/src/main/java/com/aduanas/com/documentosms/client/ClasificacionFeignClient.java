a99f90d4b40b8837fe9be77fcefc24924c781046
Estefania Ruiz <es.ruiza@duocuc.cl>
2026-05-19 23:25:54 -0400

    Se actualizaron los client de documentos.

--- documentos-ms/documentos-ms/documentos-ms/documentos-ms/src/main/java/com/aduanas/com/documentosms/client/ClasificacionFeignClient.java
+++ documentos-ms/documentos-ms/documentos-ms/documentos-ms/src/main/java/com/aduanas/com/documentosms/client/ClasificacionFeignClient.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 // Apunta al microservicio destino que calcula los impuestos y maneja el estado aduanero inmediato
-@FeignClient(name = "clasificacion-ms", url = "http://localhost:8082/api/clasificaciones")
+@FeignClient(name = "clasificacion-ms", url = "http://localhost:8082")
 public interface ClasificacionFeignClient {
 
     // Documentos solo necesita CONSULTAR (GET) los datos de la carga para validar carpetas
-    @GetMapping("/cargas/{id}")
+    @GetMapping("/api/clasificaciones/cargas/{id}")
     CargaExternaDto obtenerCargaPorId(@PathVariable("id") Long id);
 }
--- documentos-ms/documentos-ms/documentos-ms/documentos-ms/src/main/java/com/aduanas/com/documentosms/client/NotificacionFeignClient.java
+++ documentos-ms/documentos-ms/documentos-ms/documentos-ms/src/main/java/com/aduanas/com/documentosms/client/NotificacionFeignClient.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 // "notificacion-ms" es el nombre con el que se registra el microservicio destino
-@FeignClient(name = "notificacion-ms", url = "http://localhost:8085/api/notificaciones") // Ajusta el puerto real del servicio de correos
+@FeignClient(name = "notificacion-ms", url = "http://localhost:8085") // Ajusta el puerto real del servicio de correos
 public interface NotificacionFeignClient {
 
     // Cambia esto según el endpoint POST real que tus compañeros expongan para mandar correos
-    @PostMapping("/enviar-alerta")
+    @PostMapping("/api/notificaciones/enviar-alerta")
     void enviarNotificacionDocumento(
             @RequestParam("documentoId") Long documentoId,
             @RequestParam("nuevoEstado") String nuevoEstado,
