19b0d970944ff8c99c387a567927590ef459df37
fabianrubiop <fa.rubiop@duocuc.cl>
2026-06-23 19:34:47 -0400

    Add files via upload
    feat: integracion final de microservicios (Gateway, Eureka, Docker, YAML) y pruebas (JUnit5, DataFaker, Swagger, HATEOAS)

--- api-gateway/src/test/java/com/aduanas/comex/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/aduanas/comex/apigateway/ApiGatewayApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Prueba básica para verificar que el contexto de Spring carga sin errores
+    }
 
 }
